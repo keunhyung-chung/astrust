d0ec5c36bd91ee6c5e44048c1af6e80f7641319c
Keunhyung Chung <khchung@lunit.io>
2023-10-01 11:28:45 +0900

    Install `hadolint`

--- .devcontainer/post-create-commands.sh
+++ .devcontainer/post-create-commands.sh
@@ -2,5 +2,10 @@
 # Install pre-commit.
 pre-commit install-hooks && pre-commit install && pre-commit gc
 
+# Install hadolint.
+mkdir "$HOME/bin"
+curl -L -o "$HOME/bin/hadolint" https://github.com/hadolint/hadolint/releases/download/v2.12.0/hadolint-Linux-x86_64
+chmod +x "$HOME/bin/hadolint"
+
 # Check rust version.
 rustc --version
