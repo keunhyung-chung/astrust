44bdaa20f05b46a54fea9a6bc6107c12d64d4f50
Keunhyung Chung <khchung@lunit.io>
2023-10-01 12:32:15 +0900

    ⚙️ Update hadolint save path

--- .devcontainer/post-create-commands.sh
+++ .devcontainer/post-create-commands.sh
@@ -3,9 +3,9 @@
 pre-commit install-hooks && pre-commit install && pre-commit gc
 
 # Install hadolint.
-mkdir "$HOME/bin"
-curl -L -o "$HOME/bin/hadolint" https://github.com/hadolint/hadolint/releases/download/v2.12.0/hadolint-Linux-x86_64
-chmod +x "$HOME/bin/hadolint"
+mkdir -p "$HOME/.local/bin"
+curl -L -o "$HOME/.local/bin/hadolint" https://github.com/hadolint/hadolint/releases/download/v2.12.0/hadolint-Linux-x86_64
+chmod +x "$HOME/.local/bin/hadolint"
 
 # Check rust version.
 rustc --version
