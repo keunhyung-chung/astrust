13e7f0be88907f4459bea4a646371d10760edb3b
Keunhyung Chung <khchung@lunit.io>
2023-10-11 22:48:06 +0900

    Revert "Copy sample codes"
    This reverts commit d216726911636d5aea69c7f35fda219804b32c0e.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,3 @@
-use time::macros::date;
-use RustQuant::data::*;
-
 fn main() {
-    // New YahooFinanceData instance.
-    // By default, date range is: 1970-01-01 to present.
-    let mut yfd = YahooFinanceData::new("AAPL".to_string());
-
-    // Can specify custom dates (optional).
-    yfd.set_start_date(time::macros::datetime!(2019 - 01 - 01 0:00 UTC));
-    yfd.set_end_date(time::macros::datetime!(2020 - 01 - 01 0:00 UTC));
-
-    // Download the historical data.
-    yfd.get_price_history();
-
-    // Compute the returns.
-    // Specify the type of returns to compute (Simple, Logarithmic, Absolute)
-    // You don't need to run .get_price_history() first, .compute_returns()
-    // will do it for you if necessary.
-    yfd.compute_returns(ReturnsType::Logarithmic);
-
-    println!("Apple's quotes: {:?}", yfd.price_history);
-    println!("Apple's returns: {:?}", yfd.returns);
+    println!("Hello, world!");
 }
